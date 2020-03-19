513542a53507b2fec89117b0825a84808d1435fb
tomday11 <58531353+tomday11@users.noreply.github.com>
2020-03-19 15:18:33 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ myWorld = World()
 graphObj = Graph()
 
 for i in range(len(myWorld.Edges)):
-    #graphObj.add_node(myWorld.Edges[i][0])    
-    graphObj.add_edge(myWorld.Edges[i][0], myWorld.Edges[i][1],1) #graphObj.add_edge(myWorld.Edges[i][0], myWorld.Edges[i][1],myWorld.Edges[i][2]) for when we want to add actual time
+    graphObj.add_node(myWorld.Edges[i][0]) 
+
+
+for j in range(len(myWorld.Edges)):   
+    graphObj.add_edge(myWorld.Edges[j][0], myWorld.Edges[j][1],1) #graphObj.add_edge(myWorld.Edges[i][0], myWorld.Edges[i][1],myWorld.Edges[i][2]) for when we want to add actual time
 print(graphObj.__str__())
 print(graphObj.getTime(0, 10))
+for u in range(len(myWorld.Edges)):
+    print(graphObj.getNeighbor(u))
 
 
 print myWorld.Edges 
