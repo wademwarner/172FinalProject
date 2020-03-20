9f37c8b017ae47810a24eba0c5bac366dcc5d219
tomday11 <58531353+tomday11@users.noreply.github.com>
2020-03-20 14:00:59 -0400

    converts the array list of curvature to a list

--- Graph.py
+++ Graph.py
@@ -1,101 +1,219 @@
 '''
+
 Created on Mar 19, 2020
 
+
+
 @author: Wade
-'''
 
 '''
-Created on Mar 6, 2020
-@author: Thoma
+
+
+
 '''
+
+Created on Mar 6, 2020
+
+@author: Thoma
+
+'''
+
+
+import numpy
+
+
+
 
 
 
 
 class Graph(object):
+
     def __init__(self):
+
         self.dict = {} #To store the graphs nodes tht are connect with one and another
+
         self.distance = {} #to store the distance between nodes
+
         self.edge = {} #store the curvature of the edge 
+        
+        self.line = {} #store the curvature of each line
+
         '''
+
         if graphDict == None:
+
             graphDict = {}
+
         self.dict = graphDict
+
         '''
+
     
+
     
+
     def get_node_list(self): #return the vertices
+
         return list(self.dict.keys())
+
         
+
     def get_edge_list(self): #return a list of the nodes
+
         return self.genEdges()
+
     
+
     def genEdges(self):
+
         edge = []
+
         for vert in self.dict:
+
             for neib in self.dict[vert]:
+
                 edge.append({vert,neib})
+
         return edge
+
     
+
     def add_node(self, v):
+
         if v not in self.dict:
+
             self.dict[v] = []
+
     def del_node(self, v):
+
         self.dict[v] = None
+
         
-    def add_edge(self, v, w, time):  #use v as the from node and w as the to node then add the time between them as well
+
+    def add_edge(self, v, w, time, lineCurve):  #use v as the from node and w as the to node then add the time between them as well
+
         time = 1 #used only for the first part of the project
+
         if v not in self.dict:
+
             self.dict[v] = []
+
         if w not in self.dict:
+
             self.dict[w] = []   
+
         if (v,w) not in self.distance:
+
             self.dict[v].append(w)
+
             self.dict[w].append(v)
+
             self.distance[(v,w)] = time
+            
+        if (v,w) not in self.line:
+            
+            #[l.tolist() for l in list1]
+            
+            self.line[(v,w)] = [lin.tolist() for lin in lineCurve]
+            
+            
+
         
+
     def getNeighbor(self,v):
+
         if v not in self.dict:
+
             return None
+
         else:
+
             return self.dict[v]
+
     
+
     def getNeighborSize(self,v):
+
         if v not in self.dict:
+
             return None
+
         else:
+
             return len(self.dict[v])
 
-    def getTime(self,v,w):
-        return self.distance[(v,w)]
-    '''
-    likely will add this to the add_edge class then adjust it in the future for the getLine function 
-    def addLine(self,v,w,tupxY): #used to store the curve of the line 
-        self.dict[v][(v,w)].append(tupxY)
-        #self.dict[w][v].append(tupxY)
-        
     def getLine(self,v,w):
-        return self.dict[v][(v,w)][1]
-        work off tackc code more
-    '''
-    def del_edge(self, v, w):
-        self.dict[v][w] = None
-        self.dict[w][v] = None
-        if v < w:
-            
-            self.distance[(v,w)] = None
+        return self.line[(v,w)]
+    
+    def isLinStraight(self,v,w):
+        if len(self.line[(v,w)]) == 2:
+            return True
         else:
+            return False
+
+    def getTime(self,v,w):
+
+        return self.distance[(v,w)][0]
+
+    '''
+
+    likely will add this to the add_edge class then adjust it in the future for the getLine function 
+
+    def addLine(self,v,w,tupxY): #used to store the curve of the line 
+
+        self.dict[v][(v,w)].append(tupxY)
+
+        #self.dict[w][v].append(tupxY)
+
+        
+
+    def getLine(self,v,w):
+
+        return self.dict[v][(v,w)][1]
+
+        work off tackc code more
+
+    '''
+
+    def del_edge(self, v, w):
+
+        self.dict[v][w] = None
+
+        self.dict[w][v] = None
+
+        if v < w:
+
+            
+
+            self.distance[(v,w)] = None
+
+        else:
+
             self.distance[(w,v)] = None
+
         
+
         
+
     def __str__(self):
+
         vert = 'vertices: '
+
         for i in self.dict:
+
             vert += str(i) + ' '
+
         vert += '\nedges'
+
         for j in self.genEdges():
+
             vert += str(j) + ' '
+
         return vert
+
+
 
     #world give the truck locations
     
+
--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 from Classes.World import World
+
 from Classes.Graph import Graph
-from Classes.visGraph import visGraph
+
 from networkx.classes import graph
 
+
+
 graphObj = Graph()
+
 myWorld = World()
 
+
+
 '''
+
 for i in range(len(myWorld.Edges)):
+
     graphObj.add_node(myWorld.Edges[i][0]) #add vertices
+
 '''
+
+
 
 for j in range(len(myWorld.Edges)):   
-    graphObj.add_edge(myWorld.Edges[j][0], myWorld.Edges[j][1],1) #graphObj.add_edge(myWorld.Edges[i][0], myWorld.Edges[i][1],myWorld.Edges[i][2]) for when we want to add actual time
 
+    graphObj.add_edge(myWorld.Edges[j][0], myWorld.Edges[j][1],myWorld.Edges[j][2], myWorld.Edges[j][3]) #graphObj.add_edge(myWorld.Edges[i][0], myWorld.Edges[i][1],myWorld.Edges[i][2]) for when we want to add actual time
+    #need to change the graphing method so that if there is a list of size 2 then stright line
+    #else, if list is not 2 then keep iterating a new stright line to represent curvature
+
+print(myWorld.Edges)
+
+print('work?')
+print(graphObj.getLine(136, 141))
+print(graphObj.isLinStraight(136, 141))
 vtx = myWorld.Verticies
+
+
 
 myWorld.changeToViz(vtx)
 
+
+
 for k in range(len(myWorld.Edges)):
+
         #print(k, graphObj.getNeighbor(k))
+
         myWorld.drawGraph(k,graphObj.getNeighbor(k))
+
         
+
 myWorld.appearances()
+
+
 
 myWorld.runSimulation(10)
 
