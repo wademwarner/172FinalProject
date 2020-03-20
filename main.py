d5b3c66b319187ee14eb0e562fa62bc069806bb0
wademwarner <42790191+wademwarner@users.noreply.github.com>
2020-03-19 23:23:27 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 from Classes.World import World
-
 from Classes.Graph import Graph
+from Classes.visGraph import visGraph
+from networkx.classes import graph
 
-
-myWorld = World()
 graphObj = Graph()
+myWorld = World()
 
+'''
 for i in range(len(myWorld.Edges)):
-    graphObj.add_node(myWorld.Edges[i][0]) 
-
+    graphObj.add_node(myWorld.Edges[i][0]) #add vertices
+'''
 
 for j in range(len(myWorld.Edges)):   
     graphObj.add_edge(myWorld.Edges[j][0], myWorld.Edges[j][1],1) #graphObj.add_edge(myWorld.Edges[i][0], myWorld.Edges[i][1],myWorld.Edges[i][2]) for when we want to add actual time
-print(graphObj.__str__())
-print(graphObj.getTime(0, 10))
-for u in range(len(myWorld.Edges)):
-    print(graphObj.getNeighbor(u))
 
+vtx = myWorld.Verticies
 
-print myWorld.Edges 
+myWorld.changeToViz(vtx)
 
-print myWorld.Verticies
-
-#initalize a grpah object
-#add all the stuff
-#try and print it all
-
+for k in range(len(myWorld.Edges)):
+        #print(k, graphObj.getNeighbor(k))
+        myWorld.drawGraph(k,graphObj.getNeighbor(k))
+        
+myWorld.appearances()
 
 myWorld.runSimulation(10)
 
