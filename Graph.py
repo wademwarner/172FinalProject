8b0d73a1f546f071b23fe8c0a872f390967edd85
wademwarner <42790191+wademwarner@users.noreply.github.com>
2020-03-27 01:09:06 -0400

    Add files via upload

--- Graph.py
+++ Graph.py
@@ -1,21 +1,13 @@
 '''
-
 Created on Mar 19, 2020
-
-
-
 @author: Wade
-
 '''
 
 
 
 '''
-
 Created on Mar 6, 2020
-
 @author: Thoma
-
 '''
 
 
@@ -40,13 +32,9 @@ class Graph(object):
         self.line = {} #store the curvature of each line
 
         '''
-
         if graphDict == None:
-
             graphDict = {}
-
         self.dict = graphDict
-
         '''
 
     
@@ -93,7 +81,7 @@ class Graph(object):
 
     def add_edge(self, v, w, time, lineCurve):  #use v as the from node and w as the to node then add the time between them as well
 
-        #time = 1 #used only for the first part of the project
+        time = 1 #used only for the first part of the project
 
         if v not in self.dict:
 
@@ -107,21 +95,19 @@ class Graph(object):
 
             self.dict[v].append(w)
 
-            #self.dict[w].append(v)         Removed to make this to make the path test element easier 
+            self.dict[w].append(v)
 
             self.distance[(v,w)] = time
             
         if (v,w) not in self.line:
             
-           
+            #[l.tolist() for l in list1]
             
             self.line[(v,w)] = [lin.tolist() for lin in lineCurve]
             
             
 
-    def getAllNeighbors(self):
-        return self.dict.keys()    
-    
+        
 
     def getNeighbor(self,v):
 
@@ -146,7 +132,10 @@ class Graph(object):
             return len(self.dict[v])
 
     def getLine(self,v,w):
-        return self.line[(v,w)]
+        try:
+            return self.line[(v,w)]
+        except KeyError:
+            return 152
     
     def isLinStraight(self,v,w):
         if len(self.line[(v,w)]) == 2:
@@ -155,32 +144,18 @@ class Graph(object):
             return False
 
     def getTime(self,v,w):
-        if (v,w) in self.distance.keys():
-            return self.distance[(v,w)]
-        else:
-            return None
-    
-    def getAllTime(self):
-        return self.distance.values()
+
+        return self.distance[(v,w)][0]
 
     '''
-
     likely will add this to the add_edge class then adjust it in the future for the getLine function 
-
     def addLine(self,v,w,tupxY): #used to store the curve of the line 
-
         self.dict[v][(v,w)].append(tupxY)
-
         #self.dict[w][v].append(tupxY)
-
         
-
     def getLine(self,v,w):
-
         return self.dict[v][(v,w)][1]
-
         work off tackc code more
-
     '''
 
     def del_edge(self, v, w):
@@ -223,4 +198,3 @@ class Graph(object):
 
     #world give the truck locations
     
-
--- World.py
+++ World.py
@@ -2,13 +2,13 @@ from  Classes.AbstractWorld import AbstractWorld
 from Classes.Graph import Graph
 
 import pygame
+
+#from main import graphObj
 pygame.font.init() 
 
-class World(AbstractWorld):
+class World(AbstractWorld,Graph):
 	
-	graphViz = Graph()
-	
-	def __init__(self):
+	def __init__(self,x):
 		AbstractWorld.__init__(self)
 		
 		self.height = 600
@@ -27,7 +27,11 @@ class World(AbstractWorld):
 		self.neighborPositDict = {} 
 		self.rectList = []
 		self.neighborPosit = []
-
+		self.prevNode = []
+		self.line_CURV = []
+		self.line_graph = []
+		self.line_list = {}
+		self.graphViz = x
 
 	def runSimulation(self, fps=1, initialTime=5*60, finalTime=23*60):
 
@@ -73,12 +77,11 @@ class World(AbstractWorld):
 			
 		for i in verts:
 			self.vID.append(i)
-		print self.vID
 		for i in range(len(self.vID)):
 			self.vID[i][1] = int(self.vID[i][1]*self.width)
 			
 		for i in range(len(self.vID)):
-			self.vID[i][2] = int(self.vID[i][2]*self.width)
+			self.vID[i][2] = int(self.vID[i][2]*self.height)
 			
 		for i in self.vID:
 			self.vtxPosit[i[0]] = (i[1],i[2])
@@ -88,11 +91,13 @@ class World(AbstractWorld):
 	def appearances(self): #put vertices on display
 		for vertex in self.vtxPosit: #every vertex
 			pygame.draw.rect(self.screen, self.blue,(self.vtxPosit[vertex][0]-2,self.vtxPosit[vertex][1]-2,4,4))	
-		for edge in self.neighborPosit:
-			pygame.draw.aaline(self.screen,self.blue,edge[0],edge[1],4)
-			
+		for edge in self.line_list: #self.neighborPosit
+			print edge
+			for i in range(0,len(self.line_list[edge])-2):
+				pygame.draw.aaline(self.screen,self.blue,self.line_list[edge][i],self.line_list[edge][i+1],4)
+							
 		pygame.display.update()
-		pygame.time.delay(5000)
+		pygame.time.delay(10000)
 		#get neighbors of vertices and draw a line
 			
 		
@@ -102,14 +107,60 @@ class World(AbstractWorld):
 			
 		if node_neighbors != None:		
 			vertPoint = self.vtxPosit[node]
-	
-			for i in node_neighbors:
+			self.prevNode = []
+			for nn in node_neighbors: #nn -> neighbor node
+				if nn in self.prevNode or node == nn:
+					continue
+				vertNeighbor = self.vtxPosit[nn]
+				edge_init = (node,nn)
+				#print(edge_init)
+				self.line_CURV=None
+				self.line_CURV = []
+				self.line_graph = []
+				list_curV = self.graphViz.getLine(node,nn) #list of points to give the graph its "curV" (V -> vertex)
+				if list_curV == 152:
+					continue
+				for curv in list_curV:
+					list_curV_CTVX = (curv[0]*self.width, curv[1]*self.height)
+					self.line_CURV.append(list_curV_CTVX)
+				'''if vertPoint[0] <= vertNeighbor[0]:
+					selection sort left to right
+					for i in range(len(self.line_CURV)):
+						min_index = i
+						for j in range(i+1, len(self.line_CURV)):
+							if self.line_CURV[i][0] > self.line_CURV[j][0]:
+								min_index = j
+						self.line_CURV[i], self.line_CURV[min_index] =  self.line_CURV[min_index], self.line_CURV[i]
+				else:
+					maxsort to get left to right
+					size = len(self.line_CURV)-1
+					for i in range(0,size):
+						max_index = 0
+						for j in range(0,(size-i)+1):
+							if self.line_CURV[j][0] > self.line_CURV[max_index][0]:
+								max_index = j
+						self.line_CURV[size-i],self.line_CURV[max_index]=self.line_CURV[max_index], self.line_CURV[size-i]
+				'''
+				#print(self.line_CURV)
+				self.line_graph.append(vertPoint) #stores point of start vertex as first point to be lined
+				for curv in self.line_CURV:
+					self.line_graph.append(curv) #CHECK ME
+				self.line_graph.append(vertNeighbor)
+				allcurVtx_inOrder = self.line_graph
+				self.line_list[edge_init] = allcurVtx_inOrder
+				#for entry in self.line_list:
+					#print(entry, self.line_list.get(entry))
+				self.prevNode.append(nn)
+				#print(self.line_list)
+
+				
 				#print(node, "->", i)
 				#print("######")
+				'''
 				vertNeighbor = self.vtxPosit[i] #[node_neighbors[i]
 				self.neighborPosit.append((vertPoint, vertNeighbor)) #might need to be fixed for later updates, this just lists all edge points for visualization
 				#self.lineList.append(lineCreate)
+				'''
 
 
-		
 		
