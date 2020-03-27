0c95748a6d126bb856da6f2bfc8e90561e61274b
tomday11 <58531353+tomday11@users.noreply.github.com>
2020-03-27 15:32:10 -0400

    Add files via upload

--- Graph.py
+++ Graph.py
@@ -1,13 +1,21 @@
 '''
+
 Created on Mar 19, 2020
+
+
+
 @author: Wade
-'''
-
-
 
 '''
+
+
+
+'''
+
 Created on Mar 6, 2020
+
 @author: Thoma
+
 '''
 
 
@@ -32,9 +40,13 @@ class Graph(object):
         self.line = {} #store the curvature of each line
 
         '''
+
         if graphDict == None:
+
             graphDict = {}
+
         self.dict = graphDict
+
         '''
 
     
@@ -81,7 +93,7 @@ class Graph(object):
 
     def add_edge(self, v, w, time, lineCurve):  #use v as the from node and w as the to node then add the time between them as well
 
-        time = 1 #used only for the first part of the project
+        #time = 1 #used only for the first part of the project
 
         if v not in self.dict:
 
@@ -95,106 +107,262 @@ class Graph(object):
 
             self.dict[v].append(w)
 
-            self.dict[w].append(v)
+            #self.dict[w].append(v)         Removed to make this to make the path test element easier 
 
             self.distance[(v,w)] = time
-            
-        if (v,w) not in self.line:
-            
-            #[l.tolist() for l in list1]
-            
-            self.line[(v,w)] = [lin.tolist() for lin in lineCurve]
-            
-            
 
-        
 
+    def getAllNeighbors(self):
+        return self.dict.keys()    
+ 
+
+    
+    def getAllTime(self):
+        return self.distance.values()
+
+    
     def getNeighbor(self,v):
+
+
 
         if v not in self.dict:
 
+
+
             return None
 
+
+
         else:
+
+
 
             return self.dict[v]
 
+
+
     
+
+
 
     def getNeighborSize(self,v):
 
+
+
         if v not in self.dict:
+
+
 
             return None
 
+
+
         else:
+
+
 
             return len(self.dict[v])
 
+
+
     def getLine(self,v,w):
+
         try:
+
             return self.line[(v,w)]
+
         except KeyError:
+
             return 152
+
     
+
     def isLinStraight(self,v,w):
+
         if len(self.line[(v,w)]) == 2:
+
             return True
+
         else:
+
             return False
+
+
 
     def getTime(self,v,w):
 
-        return self.distance[(v,w)][0]
+
+
+        return self.distance[(v,w)]
+
+
 
     '''
+
     likely will add this to the add_edge class then adjust it in the future for the getLine function 
+
     def addLine(self,v,w,tupxY): #used to store the curve of the line 
+
         self.dict[v][(v,w)].append(tupxY)
+
         #self.dict[w][v].append(tupxY)
+
         
+
     def getLine(self,v,w):
+
         return self.dict[v][(v,w)][1]
+
         work off tackc code more
+
     '''
+
+
 
     def del_edge(self, v, w):
 
+
+
         self.dict[v][w] = None
+
+
 
         self.dict[w][v] = None
 
+
+
         if v < w:
+
+
 
             
 
+
+
             self.distance[(v,w)] = None
+
+
 
         else:
 
+
+
             self.distance[(w,v)] = None
 
-        
+
 
         
+
+
+
+        
+
+
 
     def __str__(self):
 
+
+
         vert = 'vertices: '
+
+
 
         for i in self.dict:
 
+
+
             vert += str(i) + ' '
+
+
 
         vert += '\nedges'
 
+
+
         for j in self.genEdges():
+
+
 
             vert += str(j) + ' '
 
+
+
         return vert
+
+    def find_path(self):
+        smallestNode = 'A'
+
+        C = np.max([i for i in g.getAllTime()])
+        n = len(g.getAllNeighbors())
+        
+        startNode = 'A'
+        
+        endNode = 'H'
+        path = {}
+        
+        bucket = [ [] for j in range(C*n + 1)]
+        
+        for t in g.getAllNeighbors():
+            if t == startNode:
+                path[t] = 0
+            else:
+                path[t] = n*C 
+                
+            bucket[ path[t] ].append(t)
+            
+        
+        permanentNode = {}
+        
+        before = {}
+        
+        bucketIndex = 0
+        while len(permanentNode) < n:
+            
+            if len(bucket[bucketIndex]) > 0:
+                smallestNode = bucket[bucketIndex].pop()
+                
+            else:
+                bucketIndex += 1
+                continue
+            
+            permanentNode[smallestNode] = True
+            
+            for u in g.getNeighbor(smallestNode):
+                
+                if u not in permanentNode:
+                    newDist = path[smallestNode] + g.getTime(smallestNode, u)
+                    
+                    if newDist < path[u]:
+                        bucket[ path[u] ].remove(u)
+                        path[u] = newDist
+                        before[u] = smallestNode
+                        bucket[ path[u] ].append(u)
+                        
+                        
+        print(path)
+        
+        print(before)   #want to return path and before, path
+        
+        #find the shortest path 
+        
+        done = True
+        shortestPath = {}
+        shortestPath[endNode] = [before[endNode],path[endNode]]
+        endNode = before[endNode]
+        while done:
+            if path[endNode] == 0:
+                done = False
+                shortestPath[endNode] = [endNode,0]
+            else:
+                shortestPath[endNode] = [before[endNode],path[endNode]]
+                endNode = before[endNode]
+            
+         
+        print(shortestPath)
+
+
 
 
 
     #world give the truck locations
-    
+
